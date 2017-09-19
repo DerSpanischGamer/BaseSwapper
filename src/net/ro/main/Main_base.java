99c75f4134223ee8b884140b51635430954aca30
DerSpanischGamer <robertogc99@hotmail.com>
2017-09-19 18:22:48 +0200

    Botones e inputs

--- src/net/ro/main/Main_base.java
+++ src/net/ro/main/Main_base.java
@@ -1,5 +1,7 @@
 package net.ro.main;
 
+import java.awt.Dimension;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,7 +17,18 @@ public class Main_base
 	JLabel txt_;
 	public JTextField input_;
 	
+	public JTextField num_uno;
+	
+	public JButton sum;
+	public JButton res;
+	public JButton mul;
+	public JButton div;
+	
+	public JTextField num_dos;
+	
 	JLabel debug;
+	
+	private final Dimension dim = new Dimension(50, 50);
 	
     void init_ventana()
     {
@@ -33,7 +46,7 @@ public class Main_base
         txt.setLocation(10, 10);
         txt.setSize(500, 25);
         
-        input = new JTextField("0,1,2,3,4,5,6,7,8,9");
+        input = new JTextField("0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F");
         
         input.setLocation(10, 50);
         input.setSize(500, 25);
@@ -50,6 +63,46 @@ public class Main_base
         input_.setLocation(10, 140);
         input_.setSize(500, 25);
         
+        input_.setEnabled(false);
+        
+        // Primer numero
+        
+        num_uno = new JTextField();
+        
+        num_uno.setLocation(10, 180);
+        num_uno.setSize(250, 25);
+        
+        // Segundo numero
+        
+        num_dos = new JTextField();
+
+        num_dos.setLocation(505, 180);
+        num_dos.setSize(250, 25);
+        
+        // Botones
+        
+        sum = new JButton("+");
+        
+        sum.setLocation(275, 180);
+        sum.setSize(dim);
+        
+        sum.setEnabled(false);
+        
+        res = new JButton("-");
+        
+        res.setLocation(330, 180);
+        res.setSize(dim);
+        
+        mul = new JButton("*");
+        
+        mul.setLocation(385, 180);
+        mul.setSize(dim);
+
+        div = new JButton("/");
+        
+        div.setLocation(440, 180);
+        div.setSize(dim);
+        
         // Debug para que se puedan ver bien los JTextFields
         
         debug = new JLabel();
@@ -59,6 +112,14 @@ public class Main_base
 
         ventana.add(txt_);
         ventana.add(input_);
+        
+        ventana.add(num_uno);
+        ventana.add(num_dos);
+
+        ventana.add(sum);
+        ventana.add(res);
+        ventana.add(mul);
+        ventana.add(div);
         
         ventana.add(debug);
         
